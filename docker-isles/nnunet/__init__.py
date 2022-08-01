8fd03455a88c13306c2ceea1d18c2c5a37d6b126
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2022-08-01 11:25:57 +0100

    Try fixing nnunet import

---
+++ docker-isles/nnunet/__init__.py
@@ -0,0 +1,7 @@
+from . import drop
+from . import loss
+from . import metrics
+from . import model
+from . import modules
+from . import nn_unet
+from . import shape
---
+++ docker-isles/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='nnunet',
+    version='0.0.1',
+    packages=['nnunet'],
+    author='PLORAS',
+    install_requires=[
+        'torch'
+    ],
+)
